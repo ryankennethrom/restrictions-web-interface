514fda25dbcbf0c3cf3d4622897f47366dd5d306
Ryan Rom <ryankennethrom@gmail.com>
2025-12-23 06:04:59 -0700

    Update folder path for checksum

--- api/checksum.js
+++ api/checksum.js
@@ -2,34 +2,36 @@ import crypto from 'crypto';
 import fs from 'fs';
 import path from 'path';
 
+const FOLDER_PATH = process.cwd(); // root of the project (entire restrictions-web-interface)
+
+function hashFolder(folderPath) {
+  const hash = crypto.createHash('sha256');
+
+  function walkDir(dir) {
+    const entries = fs.readdirSync(dir).sort();
+    for (const entry of entries) {
+      const fullPath = path.join(dir, entry);
+      const stat = fs.statSync(fullPath);
+
+      if (stat.isDirectory()) {
+        walkDir(fullPath);
+      } else if (stat.isFile()) {
+        const fileBuffer = fs.readFileSync(fullPath);
+        hash.update(fileBuffer);
+      }
+    }
+  }
+
+  walkDir(folderPath);
+  return hash.digest('hex');
+}
+
 export default async function handler(req, res) {
   try {
-    function hashFolder(folderPath) {
-      const hash = crypto.createHash('sha256');
-
-      function walkDir(dir) {
-        const entries = fs.readdirSync(dir).sort(); // Sort for deterministic hash
-        for (const entry of entries) {
-          const fullPath = path.join(dir, entry);
-          const stat = fs.statSync(fullPath);
-
-          if (stat.isDirectory()) {
-            walkDir(fullPath);
-          } else if (stat.isFile()) {
-            const fileBuffer = fs.readFileSync(fullPath);
-            hash.update(fileBuffer);
-          }
-        }
-      }
-
-      walkDir(folderPath);
-      return hash.digest('hex');
-    }
-
-    const folderHash = hashFolder('../../restrictions-web-interface');
-
+    const folderHash = hashFolder(FOLDER_PATH);
     res.status(200).json({ folderHash });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 }
+
